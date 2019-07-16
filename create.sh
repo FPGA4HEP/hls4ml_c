bdd5d7275318681c8d920f815cece509acc5a39e
Javier Duarte <jduarte1@fnal.gov>
2019-07-16 08:18:02 +0000

    update

--- create.sh
+++ create.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 binname=`ls xclbin/ | grep "\.xclbin" | grep "\.hw\." | sed 's/\.xclbin//' | grep -v 'info'`
-$SDACCEL_DIR/tools/create_sdaccel_afi.sh -xclbin=xclbin/$binname.xclbin -o=$binname -s3_bucket=fpga4hep-afi-3 -s3_dcp_key=inputfiles -s3_logs_key=logs
+$SDACCEL_DIR/tools/create_sdaccel_afi.sh -xclbin=xclbin/$binname.xclbin -o=$binname -s3_bucket=fnal-afi -s3_dcp_key=inputfiles -s3_logs_key=logs
