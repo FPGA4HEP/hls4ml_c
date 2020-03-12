d12b0fe8a3816f5a8723b67e9348c5b35a39121e
Dylan Rankin <dylansr90@gmail.com>
2020-03-12 12:48:45 +0000

    updating for new AMI (2019.2)

---
+++ src/weights/b6.h
@@ -0,0 +1,15 @@
+//Numpy array shape [1]
+//Min -0.230036392808
+//Max -0.230036392808
+//Number of zeros 0
+
+#ifndef B6_H_
+#define B6_H_
+
+#ifndef __SYNTHESIS__
+model_default_t b6[1];
+#else
+model_default_t b6[1] = {-0.230036};
+#endif
+
+#endif
