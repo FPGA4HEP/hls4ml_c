f94069d73a6b0a8e47e658e5ad14a1fd98717661
Dylan Rankin <dylansr90@gmail.com>
2020-03-12 19:46:00 +0000

    fixing for vitis 2019.2

--- create.sh
+++ create.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-binname=`ls xclbin/ | grep "\.xclbin$" | grep "\.hw\." | sed 's/\.xclbin//'`
-$SDACCEL_DIR/tools/create_sdaccel_afi.sh -xclbin=xclbin/$binname.xclbin -o=$binname -s3_bucket=fnal-afi -s3_dcp_key=inputfiles -s3_logs_key=logs
+binname=`ls build_dir*/ | grep "\.xclbin$" | sed 's/\.xclbin//'`
+dirname=`ls . | grep "build_dir"`
+echo $dirname
+echo $binname
+$VITIS_DIR/tools/create_vitis_afi.sh -xclbin=$dirname/$binname.xclbin -o=$binname -s3_bucket=fnal-afi -s3_dcp_key=inputfiles -s3_logs_key=logs
